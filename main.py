9cdac7039857a0b45ecd80305778415c4d1c2539
TikhonKrivolutskiy <152998914+TikhonKrivolutskiy@users.noreply.github.com>
2024-03-03 23:48:43 +0700

    Commit №2
    Task #2

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('base.html', title='Заготовка')
+    return render_template('base.html', title='Готовимся к миссии')
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', title='Тренировки в полёте', occ=prof.lower())   
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
