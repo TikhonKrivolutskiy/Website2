d3315af13286228bb0d15446b79d353b94825ae9
TikhonKrivolutskiy <152998914+TikhonKrivolutskiy@users.noreply.github.com>
2024-03-04 15:03:58 +0700

    Commit №3
    Task #3

--- main.py
+++ main.py
@@ -11,5 +11,10 @@ def index():
 def training(prof):
     return render_template('training.html', title='Тренировки в полёте', occ=prof.lower())   
 
+@app.route('/list_prof/<atr>')
+def list_prof(atr):
+    return render_template('list_prof.html', title='Список профессий', atr=atr)   
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
