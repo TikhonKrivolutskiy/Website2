9cbd5359cbb82e5a53fb8fcb466a1402c683373c
TikhonKrivolutskiy <152998914+TikhonKrivolutskiy@users.noreply.github.com>
2024-03-11 22:30:14 +0700

    Commit №6
    Task #6

--- main.py
+++ main.py
@@ -60,6 +60,10 @@ def login():
         return redirect('/success')
     return render_template('login.html', title='Аварийный доступ', form=form)
 
+@app.route('/distribution')
+def distribution():
+    return render_template('distribution.html', title='По каютам!')
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
